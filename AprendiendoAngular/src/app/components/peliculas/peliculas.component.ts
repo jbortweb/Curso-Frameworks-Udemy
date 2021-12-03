7509cd93e2da2c7ad35e07e4c954ac95a4c6cc87
jbortweb <jbortweb@gmail.com>
2021-12-03 11:50:03 +0100

    ngClass y ng-templates

--- AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -9,16 +9,17 @@ export class PeliculasComponent implements OnInit, DoCheck {
 
   public title: string;
   public peliculas!: Array<any>;
+  public year!: number;
 
   constructor() { 
     
     this.title = 'Películas';
     this.peliculas = [
-      {titulo: "Spiderman 4", image:'https://www.nacionflix.com/__export/1635039461533/sites/debate/img/2021/10/23/sony-spiderman-4.jpg_1902800913.jpg'},
+      {year: 2021, titulo: "Spiderman 4", image:'https://www.nacionflix.com/__export/1635039461533/sites/debate/img/2021/10/23/sony-spiderman-4.jpg_1902800913.jpg'},
 
-      {titulo: "Los vengadores Endgame", image:'https://i1.wp.com/hipertextual.com/wp-content/uploads/2019/04/hipertextual-nuevo-trailer-avengers-endgame-agradece-fans-universo-marvel-2019351167.jpg?fit=1600%2C900&ssl=1'},
+      {year: 2020, titulo: "Los vengadores Endgame", image:'https://i1.wp.com/hipertextual.com/wp-content/uploads/2019/04/hipertextual-nuevo-trailer-avengers-endgame-agradece-fans-universo-marvel-2019351167.jpg?fit=1600%2C900&ssl=1'},
 
-      {titulo: "Superman vs Batman", image:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/batman-v-superman-el-amanecer-de-la-justicia-2016-ben-affleck-y-henry-cavill-1558006115.jpg?crop=1xw:0.8880994671403197xh;center,top&resize=1200:*'}
+      {year: 2019, titulo: "Superman vs Batman", image:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/batman-v-superman-el-amanecer-de-la-justicia-2016-ben-affleck-y-henry-cavill-1558006115.jpg?crop=1xw:0.8880994671403197xh;center,top&resize=1200:*'}
     ];
    
   }
