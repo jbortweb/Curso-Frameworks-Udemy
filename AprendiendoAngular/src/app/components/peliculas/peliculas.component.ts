7b4960dea76795d82255962c935a51212f3a0763
jbortweb <jbortweb@gmail.com>
2021-12-21 12:26:39 +0100

    Slider personalizado

--- AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { Pelicula } from '../../models/pelicula';
 
 @Component({
   selector: 'peliculas',
@@ -8,20 +9,19 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 export class PeliculasComponent implements OnInit, DoCheck {
 
   public title: string;
-  public peliculas!: Array<any>;
-  public year!: number;
+  public peliculas: Pelicula[];
+  
 
   constructor() { 
     
     this.title = 'Películas';
     this.peliculas = [
-      {year: 2021, titulo: "Spiderman 4", image:'https://www.nacionflix.com/__export/1635039461533/sites/debate/img/2021/10/23/sony-spiderman-4.jpg_1902800913.jpg'},
+      new Pelicula('Spiderman 4', 2021, 'https://www.nacionflix.com/__export/1635039461533/sites/debate/img/2021/10/23/sony-spiderman-4.jpg_1902800913.jpg'),
 
-      {year: 2020, titulo: "Los vengadores Endgame", image:'https://i1.wp.com/hipertextual.com/wp-content/uploads/2019/04/hipertextual-nuevo-trailer-avengers-endgame-agradece-fans-universo-marvel-2019351167.jpg?fit=1600%2C900&ssl=1'},
+      new Pelicula('Los vengadores Endgame', 2020, 'https://i1.wp.com/hipertextual.com/wp-content/uploads/2019/04/hipertextual-nuevo-trailer-avengers-endgame-agradece-fans-universo-marvel-2019351167.jpg?fit=1600%2C900&ssl=1'),
 
-      {year: 2019, titulo: "Superman vs Batman", image:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/batman-v-superman-el-amanecer-de-la-justicia-2016-ben-affleck-y-henry-cavill-1558006115.jpg?crop=1xw:0.8880994671403197xh;center,top&resize=1200:*'}
-    ];
-   
+      new Pelicula('Superman vs Batman', 2019, 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/batman-v-superman-el-amanecer-de-la-justicia-2016-ben-affleck-y-henry-cavill-1558006115.jpg?crop=1xw:0.8880994671403197xh;center,top&resize=1200:*')
+    ];   
   }
 
   ngOnInit(): void {
